4eeaef1a06c37d6380ffe4fc6d44003ed97b955c
Elena Pedko <elpg.ods@gmail.com>
2022-10-04 15:33:49 +0300

    Task one is completed

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+console.log(' ');
+categories.forEach(category => {
+    const group = [...category.children];
+    group.forEach(element => {
+        if (element.tagName === 'H2') {
+            console.log(`Category: ${element.textContent}`);
+        }
+    });
+    group.forEach(element => {
+        if (element.tagName === 'UL') {
+            console.log(`Elements: ${element.children.length}`);
+        }
+    });
+    console.log(' ')
+});
